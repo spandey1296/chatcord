f1caf2a6a9d496a1601aa374036d440e56cbc9f7
Shivant Pandey <50301680+spandey1296@users.noreply.github.com>
2023-03-24 16:22:37 +0530

    server port changes

--- server.js
+++ server.js
@@ -70,6 +70,6 @@ io.on('connection', socket => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
